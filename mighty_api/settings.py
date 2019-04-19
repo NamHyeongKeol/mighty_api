5830e0846ee30b138329662c3da6807c3d3a1ae4
NamHyeongKeol <grey_pastor@naver.com>
2019-04-19 23:13:33 +0900

    add shell_plus

--- mighty_api/settings.py
+++ mighty_api/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'mighty',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+SHELL_PLUS = "ipython"
+
